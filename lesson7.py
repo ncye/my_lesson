39b7982eefc244adc2d7ba7dca213a8f331d3f6e
ncye <isaas_box@vk.com>
2022-12-11 18:03:21 +0300

    add file

--- lesson7.py
+++ lesson7.py
@@ -6,49 +6,49 @@ import numpy as np
 
 
 # Функция создания матрицы
-def creating_matrix(row, col) -> np.ndarray:
-    matrix: np.ndarray = np.zeros((row, col))
+def creating_matrix(row, column) -> np.ndarray:
+    matrix: np.ndarray = np.zeros((row, column))
     for i in range(row):
-        for j in range(col):
+        for j in range(column):
             matrix[i, j] = float(input('Введите элемент матрицы: '))
     return matrix
 
 
 # Функция поиска максимального значения в столбце
-def maximum_column_value(matrix, row, col):
-    max = np.zeros(col)
-    for j in range(col):
-        max[j] = matrix[0, j]
+def maximum_column_value(matrix, row, column):
+    matrix_max = np.zeros(column)
+    for j in range(column):
+        matrix_max[j] = matrix[0, j]
         for i in range(row):
-            if matrix[i, j] > max[j]:
-                max[j] = matrix[i, j]
-    return max
+            if matrix[i, j] > matrix_max[j]:
+                matrix_max[j] = matrix[i, j]
+    return matrix_max
 
 
 # Функция вывода результата
-def resout(max1):
-    print('Максимальные значения для каждого столбца матрицы A: ', max1)
-    # print('Максимальные значения для каждого столбца матрицы B: ', max2)
-    # print('Максимальные значения для каждого столбца матрицы C: ', max3)
+def resout(maximum_column_value_1, maximum_column_value_2, maximum_column_value_3):
+    print('Максимальные значения для каждого столбца матрицы A: ', maximum_column_value_1)
+    print('Максимальные значения для каждого столбца матрицы B: ', maximum_column_value_2)
+    print('Максимальные значения для каждого столбца матрицы C: ', maximum_column_value_3)
 
 
 if __name__ == '__main__':
     # Создание матриц
-    row = 2
-    col = 3
-    A = creating_matrix(row, col)
+    n = 2
+    m = 3
+    A = creating_matrix(n, m)
     print(A)
-    # n = 5
-    # m = 7
-    # B = matrix(n, m)
-    # n = 3
-    # m = 4
-    # C = matrix(n, m)
+    n = 5
+    m = 7
+    B = creating_matrix(n, m)
+    n = 3
+    m = 4
+    C = creating_matrix(n, m)
 
     # Поиск максимальных значений
-    max1 = maximum_column_value(A, row, col)
-    # max2 = max_col(B, n)
-    # max3 = max_col(C, n)
+    max1 = maximum_column_value(A, n, m)
+    max2 = maximum_column_value(B, n, m)
+    max3 = maximum_column_value(C, n, m)
 
     # Вывод результата
-    resout(max1)
+    resout(max1, max2, max3)
