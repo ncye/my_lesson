a88b5d32a4a236f869736b4bb296a3e7f9b532ac
ncye <isaas_box@vk.com>
2022-12-11 17:43:29 +0300

    add file

--- lesson7.py
+++ lesson7.py
@@ -7,10 +7,10 @@ import numpy as np
 
 # Функция создания матрицы
 def matrix(row, col) -> np.ndarray:
-    matrix = np.zeros((row, col))
+    matrix: np.ndarray = np.zeros((row, col))
     for i in range(row):
         for j in range(col):
-            A[i, j] = float(input('Введите элемент матрицы: '))
+            matrix[i, j] = float(input('Введите элемент матрицы: '))
     return matrix
 
 
@@ -24,27 +24,29 @@ def max_col(matrix, n):
 
 
 # Функция вывода результата
-def resout(max1, max2, max3):
+def resout(max1):
     print('Максимальные значения для каждого столбца матрицы A: ', max1)
-    print('Максимальные значения для каждого столбца матрицы B: ', max2)
-    print('Максимальные значения для каждого столбца матрицы C: ', max3)
+    # print('Максимальные значения для каждого столбца матрицы B: ', max2)
+    # print('Максимальные значения для каждого столбца матрицы C: ', max3)
 
 
-# Создание матриц
-n = 4
-m = 5
-A = matrix(n, m)
-n = 5
-m = 7
-B = matrix(n, m)
-n = 3
-m = 4
-C = matrix(n, m)
+if __name__ == '__main__':
+    # Создание матриц
+    n = 2
+    m = 3
+    A = matrix(n, m)
+    # n = 5
+    # m = 7
+    # B = matrix(n, m)
+    # n = 3
+    # m = 4
+    # C = matrix(n, m)
 
-# Поиск максимальных значений
-max1 = max_col(A, n)
-max2 = max_col(B, n)
-max3 = max_col(C, n)
+    # Поиск максимальных значений
+    max1 = max_col(A, n)
+    # max2 = max_col(B, n)
+    # max3 = max_col(C, n)
 
-# Вывод результата
-resout(max1, max2, max3)
+    # Вывод результата
+    resout(max1)
+
