37643129dae7f069a905e8b19c4c502b8aa73537
ncye <isaas_box@vk.com>
2022-12-14 00:02:23 +0300

    11.12.22

--- Lesson9.py
+++ Lesson9.py
@@ -3,6 +3,7 @@
 Использовать GUI
 """
 import numpy as np
+from tkinter import *
 
 
 # Класс создания матрицы с случайными числами
@@ -31,3 +32,33 @@ class MaximumColumnValue(CreatingMatrix):
         return matrix_max
 
 
+# GUI интерфейс с выводом результата
+
+
+def resout(col_1, col_2, col_3):
+    root = Tk()
+    root.title('Результат')
+    root.geometry('500x300')
+    root.resizable(False, False)
+
+    label = Label(root, text='Максимальные значения для каждого столбца матрицы A: {}'.format(col_1))
+    label.pack()
+
+    label = Label(root, text='Максимальные значения для каждого столбца матрицы B: {}'.format(col_2))
+    label.pack()
+
+    label = Label(root, text='Максимальные значения для каждого столбца матрицы C: {}'.format(col_3))
+    label.pack()
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    matrix_A = CreatingMatrix(4, 5).creating_matrix()
+    matrix_B = CreatingMatrix(5, 7).creating_matrix()
+    matrix_C = CreatingMatrix(3, 4).creating_matrix()
+    maximum_column_value_A = MaximumColumnValue(4, 5).maximum_column_value(matrix_A)
+    maximum_column_value_B = MaximumColumnValue(5, 7).maximum_column_value(matrix_B)
+    maximum_column_value_C = MaximumColumnValue(3, 4).maximum_column_value(matrix_C)
+
+    resout(maximum_column_value_A, maximum_column_value_B, maximum_column_value_C)
